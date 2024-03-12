d889c2e2982490d01624d26fbfcc04f1ea345f54
Zac <97988282+wherearemysocks11@users.noreply.github.com>
2024-03-12 12:48:02 +0000

    folder clean up

--- lib/FunctionsProject.dart
+++ lib/FunctionsProject.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
 import 'dart:async';
 
-
 class Order {
   String orderId;
   String customerName;
   List<String> items;
-  
+
   Order(this.orderId, this.customerName, this.items);
 }
 
 abstract class OrderManager {
   Future<Order> getOrder(String orderId);
-  Future<void> editOrder(String orderId, String customerName, List<String> items);
+  Future<void> editOrder(
+      String orderId, String customerName, List<String> items);
 }
-
 
 class OrderManagerImpl implements OrderManager {
   final PostgreSQLConnection _connection;
@@ -31,14 +30,16 @@ class OrderManagerImpl implements OrderManager {
 
     if (results.isNotEmpty) {
       var order = results.first;
-      return Order(order[0].toString(), order[1].toString(), (order[2] as List<dynamic>).map((item) => item.toString()).toList());
+      return Order(order[0].toString(), order[1].toString(),
+          (order[2] as List<dynamic>).map((item) => item.toString()).toList());
     } else {
       return null;
     }
   }
 
   @override
-  Future<void> editOrder(String orderId, String customerName, List<String> items) async {
+  Future<void> editOrder(
+      String orderId, String customerName, List<String> items) async {
     await _connection.query(
       'UPDATE orders SET customer_name = @customerName, items = @items WHERE order_id = @orderId',
       substitutionValues: {
@@ -50,22 +51,22 @@ class OrderManagerImpl implements OrderManager {
   }
 }
 
-
 Future<void> main() async {
 //   final conn = await Connection.open(
 //   Endpoint(
-//     host: host, 
+//     host: host,
 //     database: database)
 //  )
 
-  final connection = PostgreSQLConnection('localhost', 5432, 'your_database_name',
+  final connection = PostgreSQLConnection(
+      'localhost', 5432, 'your_database_name',
       username: 'your_username', password: 'your_password');
 
   await connection.open();
 
   var orderManager = OrderManagerImpl(connection);
-  
-  // Get order Example 
+
+  // Get order Example
   var order = await orderManager.getOrder('1');
   if (order != null) {
     print('Order details:');
@@ -75,7 +76,7 @@ Future<void> main() async {
   } else {
     print('Order not found.');
   }
-  
+
   // Edit order example
   await orderManager.editOrder('1', 'Jane Smith', ['Item3', 'Item4']);
   order = await orderManager.getOrder('1');
@@ -87,32 +88,15 @@ Future<void> main() async {
   } else {
     print('Order not found.');
   }
-  
+
   await connection.close();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // void main () async {
 //  final conn = await Connection.open(
 //   Endpoint(
-//     host: host, 
+//     host: host,
 //     database: database)
 //  )
-
-
-
 
 // }
