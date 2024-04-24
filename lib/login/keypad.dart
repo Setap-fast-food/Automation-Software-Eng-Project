20965da8e3c732624f6b8bd21d360c917b1e2566
JamieB0 <128060563+JamieB0@users.noreply.github.com>
2024-04-24 15:30:46 +0100

    Login Page

---
+++ lib/login/keypad.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class NumericKeypad extends StatefulWidget {
+  final TextEditingController controller;
+
+  const NumericKeypad({super.key, required this.controller});
+
+  @override
+  State<NumericKeypad> createState() => _NumericKeypadState();
+}
+
+class _NumericKeypadState extends State<NumericKeypad> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // 1
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            _buildButton('1'),
+            _buildButton('2'),
+            _buildButton('3'),
+          ],
+        ),
+        Row(
+          children: [
+            _buildButton('4'),
+            _buildButton('5'),
+            _buildButton('6'),
+          ],
+        ),
+        Row(
+          children: [
+            _buildButton('7'),
+            _buildButton('8'),
+            _buildButton('9'),
+          ],
+        ),
+        Row(
+          children: [
+            _buildButton('Clear', onPressed: _clear),
+            _buildButton('0'),
+            _buildButton('⌫', onPressed: _backspace),
+          ],
+        ),
+      ],
+    );
+  }
+
+  // 2
+  // Individual keys
+  Widget _buildButton(String text, {VoidCallback? onPressed}) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: onPressed ?? () => _input(text),
+          child: Text(text),
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.grey[300]!),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _input(String text) {
+    String currentText = _controller.text;
+    String newText = currentText + text;
+    _controller.text = newText;
+  }
+
+  void _backspace() {
+    _controller.text =
+        _controller.text.substring(0, _controller.text.length - 1);
+  }
+
+  void _clear() {
+    _controller.text = '';
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:automation_software_flutter_code/home_page_widget.dart';
 import 'package:automation_software_flutter_code/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'payment.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'login/login.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -20,18 +20,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // 233 215 255
-        primaryColor: const Color.fromRGBO(151, 71, 255, 1),
-        // colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(151, 71, 255, 1)),
-        useMaterial3: true,
-      ),
-      routes: {
-        '/' : (context) => const WelcomePage(title: "Scenario Selection"),
-        '/staff_till': (context) => const PaymentPage(title: "Your Order"),
-        '/kitchen': (context) => const HomePageWidget()
-      },
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // 233 215 255
+          primaryColor: const Color.fromRGBO(151, 71, 255, 1),
+          // colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(151, 71, 255, 1)),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/': (context) => const WelcomePage(title: "Scenario Selection"),
+          '/staff_till': (context) => const PaymentPage(title: "Your Order"),
+          '/kitchen': (context) => const LoginPage(title: "Login")
+        });
   }
 }
