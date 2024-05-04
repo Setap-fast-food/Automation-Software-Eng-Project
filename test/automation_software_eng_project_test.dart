9c38f26ff5533043f3101acf8d2ec693ffeb7894
Zac <97988282+wherearemysocks11@users.noreply.github.com>
2024-05-04 21:42:39 +0100

    test name corrections

--- test/automation_software_eng_project_test.dart
+++ test/automation_software_eng_project_test.dart
@@ -5,33 +5,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
-  group('All tests + widget tests', () {
-    test('Test: placeOrder() - should be able to place an order', () async {
-      Order instance1 = Order.create(data);
-      // due to not being able to test on a live database, the
-      // only way to test is to run the code on a mock database
-      // and make sure it doesnt throw errors when executing the code
-      String x = await instance1.placeOrder(true);
-      expect(x, 'Test Success');
-      expect(instance1.name, 'JoeBlogs');
-    });
+  test('placeOrder() functionality', () async {
+    Order instance1 = Order.create(data);
+    // due to not being able to test on a live database, the
+    // only way to test is to run the code on a mock database
+    // and make sure it doesnt throw errors when executing the code
+    String x = await instance1.placeOrder(true);
+    expect(x, 'Test Success');
+    expect(instance1.name, 'JoeBlogs');
+  });
 
-    testWidgets('Test: Clear Button Functionality',
-        (WidgetTester tester) async {
-      final controller = TextEditingController();
-      controller.text = '135';
+  testWidgets('Clear Button Functionality', (WidgetTester tester) async {
+    final controller = TextEditingController();
+    controller.text = '135';
 
-      await tester.pumpWidget(MaterialApp(
-        home: NumericKeypad(controller: controller),
-      ));
-      expect(controller.text, '135');
+    await tester.pumpWidget(MaterialApp(
+      home: NumericKeypad(controller: controller),
+    ));
 
-      final clearButtonFinder = find.byKey(clearButtonKey);
+    expect(controller.text, '135');
+    final clearButtonFinder = find.byKey(clearButtonKey);
 
-      await tester.tap(clearButtonFinder);
+    await tester.tap(clearButtonFinder);
 
-      // Verify that the input text is cleared
-      expect(controller.text, '');
-    });
+    expect(controller.text, '');
   });
 }
