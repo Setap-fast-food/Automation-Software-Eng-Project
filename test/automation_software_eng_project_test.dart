350c70cbd21d8409b85294d95f4d09a52dc234fb
Zac <97988282+wherearemysocks11@users.noreply.github.com>
2024-05-04 14:05:33 +0100

    first test

--- test/automation_software_eng_project_test.dart
+++ test/automation_software_eng_project_test.dart
@@ -1,12 +1,7 @@
 import 'package:automation_software_flutter_code/payment.dart';
 import 'package:automation_software_flutter_code/Order.dart';
-import 'package:automation_software_flutter_code/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:automation_software_flutter_code/main.dart';
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-import 'package:automation_software_flutter_code/components/main_panel_widget.dart';
+;
 
 void main() async {
   group('All tests of automation_software_eng_project', () {
@@ -18,16 +13,3 @@ void main() async {
     });
   });
 }
-
-/*
-
-void main() async {
-  final instance = FakeFirebaseFirestore();
-  await instance.collection('users').add({
-    'username': 'Bob',
-  });
-  final snapshot = await instance.collection('users').get();
-  print(snapshot.docs.first.get('username')); // 'Bob'
-}
-
-*/
